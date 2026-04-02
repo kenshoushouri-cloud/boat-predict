92c9b57d6a14eb45d08bd4583a6f7c6b44c5405f
kenshoushouri-cloud <kenshou.shouri@gmail.com>
2026-04-02 13:27:36 +0900

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,5 @@
-import os
-import urllib.request
-import json
-
-url = os.environ.get("SCRIPT_URL", "")
-if url:
-    with urllib.request.urlopen(url) as r:
-        exec(r.read().decode())
-else:
-    print("SCRIPT_URL not set")
+import os,sys,urllib.request
+u=os.environ.get("RAW_URL","")
+if u:
+    code=urllib.request.urlopen(u).read().decode("utf-8")
+    exec(compile(code,"scheduler","exec"))
