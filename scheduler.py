5e18cd3b1978a2b63c6fa080bac52450a9aae862
kenshoushouri-cloud <kenshou.shouri@gmail.com>
2026-04-02 14:11:52 +0900

    scheduler.py を更新

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,4 @@
-import os,sys,urllib.request
-u=os.environ.get("RAW_URL","")
-if u:
-    code=urllib.request.urlopen(u).read().decode("utf-8")
-    exec(compile(code,"scheduler","exec"))
+import  time
+print("start")
+while true:
+    time.sleep(60)
